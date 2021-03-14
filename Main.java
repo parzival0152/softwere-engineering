eb5425893533670b1e7aed0d47fafcebf4a313a1
parzival0152 <ilay.tzu@gmail.com>
2021-03-14 12:50:50 +0200

    added menu
    added menu and switching logic
    TODO: add more options to the switch

--- Main.java
+++ Main.java
@@ -1,19 +1,58 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.Collections;
 
 public class Main
 {
     public static void main(String[] args) {
-        ArrayList<Integer> numlist = new ArrayList<Integer>();
-        numlist.add(5);
-        numlist.add(4);
-        numlist.add(3);
-        numlist.add(2);
-        numlist.add(1);
-        Collections.sort(numlist);
-        for(int i:numlist)
+        Scanner input = new Scanner(System.in);
+        int option;
+        boolean exit = false;
+        while (!exit)
         {
-            System.out.println(i*i);
+            //print out the start menu
+            System.out.println("Please choose one of the following options: ");
+            System.out.println("1. Add new contact");
+            System.out.println("2. Remove a contact by name");
+            System.out.println("3. Print all contacts");
+            System.out.println("4. Find contact by name");
+            System.out.println("5. Sort contact list by name");
+            System.out.println("6. Sort contact list by number");
+            System.out.println("7. Remove duplicates");
+            System.out.println("8. Sort in reverse");
+            System.out.println("9. Save to file");
+            System.out.println("10. Load from file");
+            System.out.println("11. exit");
+            //get choice
+            System.out.print("Your choice: ");
+            option = Integer.parseInt(input.nextLine());
+            //cls
+            clearScreen();
+            //switch based on the option that they chose
+            switch (option) {
+                case 1:
+                    //add new contact;
+                    System.out.println("you choose 1");
+                    break;
+
+                case 2:
+                    //remove by name
+                    System.out.println("you choose 2");
+                    break;
+
+                case 11:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Error: not an option");
+                    break;
+            }
         }
+        input.close();
     }
+
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    } 
 }
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # softwere engineering
- 
-<body>
-<image href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F137500594847523011%2F&psig=AOvVaw36B7XnuoI-0BXPPGJHe49C&ust=1615803237608000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPCQo6HGr-8CFQAAAAAdAAAAABAD">
-</body>
+
+this is the repository for our work on the course software engineering
+:tada::tada::tada::tada::tada: :fireworks:
