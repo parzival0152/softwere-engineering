3dbe63f3d367ea0d83cf236a1e98fa269031b75c
parzival0152 <ilay.tzu@gmail.com>
2021-03-14 14:38:11 +0200

    Done options 1-4,11

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-// import java.util.ArrayList;
+import java.util.ArrayList;
 // import java.util.Collections;
 
 public class Main
@@ -8,8 +8,12 @@ public class Main
         Scanner input = new Scanner(System.in);
         int option;
         boolean exit = false;
+        String name;
+        String number;
+        ArrayList<Contact> contactList = new ArrayList<>();
         while (!exit)
         {
+            
             //print out the start menu
             System.out.println("Please choose one of the following options: ");
             System.out.println("1. Add new contact");
@@ -32,18 +36,79 @@ public class Main
             switch (option) {
                 case 1:
                     //add new contact;
-                    System.out.println("you choose 1");
+                    System.out.print("Enter contact name: ");
+                    name = input.nextLine();
+                    System.out.print("Enter contact number: ");
+                    number = input.nextLine();
+                    contactList.add(new Contact(name, number));
                     break;
 
                 case 2:
                     //remove by name
-                    System.out.println("you choose 2");
+                    System.out.print("Enter contact name: ");
+                    name = input.nextLine();
+                    for (int i = 0; i < contactList.size(); i++) {
+                        if(name.equals(contactList.get(i).name))
+                        {
+                            contactList.remove(i);
+                            break;
+                        }
+                    }
                     break;
 
+                case 3:
+                    //print all contacts
+                    for(Contact c: contactList)
+                    {
+                        System.out.println(c);
+                    }
+                    input.nextLine();
+                    break;
+
+                case 4:
+                    //find contact by name
+                    System.out.print("Enter contact name: ");
+                    name = input.nextLine();
+                    for (int i = 0; i < contactList.size(); i++) {
+                        if(name.equals(contactList.get(i).name))
+                        {
+                            System.out.println(contactList.get(i));
+                        }
+                    }
+                    input.nextLine();
+                    break;
+
+                case 5:
+                    //sort contact list by name
+                    break;
+
+                case 6:
+                    //sort contact list by number
+                    break;
+
+                case 7:
+                    //remove dups
+                    break;
+
+                case 8:
+                    //sort in reverse
+                    break;
+
+                case 9:
+                    //save to file
+                    break;
+
+                case 10:
+                    //read from file
+                    break;
+                 
                 case 11:
+                    //quit
                     exit = true;
                     break;
+
                 default:
+                    //default cause we have to have one
                     System.out.println("Error: not an option");
                     break;
             }
@@ -53,6 +118,20 @@ public class Main
 
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
-        System.out.flush();  
+        System.out.flush();
     } 
+}
+
+class Contact{
+    String name;
+    String number;
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String toString()
+    {
+        return this.name + " : "+this.number;
+    }
 }
