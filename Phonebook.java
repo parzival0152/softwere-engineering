91acdb5b6e6130e70c95c132d684195a747280a5
eliyabron <eliyabron@gmail.com>
2021-05-03 19:53:24 +0300

    Update Phonebook.java

--- Phonebook.java
+++ Phonebook.java
@@ -40,78 +40,46 @@ public class Phonebook extends App
                     //add new contact;
                     System.out.print("Enter contact name: ");
                     name = Input.nextLine();
-                    //find if contact already exists
-                    if (contactList!=null)
-                    {
-                        if(findContact(name)!=-1)
-                        {
-                            System.out.print("Contact already exists.");
-                            break;
-                        }                        
-                    }
-
-
                     System.out.print("Enter contact number: ");
                     number = Input.nextLine();
-                    contactList.add(new Contact(name, number));
-                    set(contactList);
-                    break;
+                    addContact(name,number);
 
                 case 2:
                     //remove by name
                     System.out.print("Enter contact name: ");
                     name = Input.nextLine();
-                    for (int i = 0; i < contactList.size(); i++) {
-                        if(name.equals(contactList.get(i).name))
-                        {
-                            contactList.remove(i);
-                            break;
-                        }
-                    }
-                    set(contactList);
+                    removeContact(name);
                     update();
                     
                     break;
 
                 case 3:
-                    //print all contacts
-                    for(Contact c: contactList)
-                    {
-                        System.out.println(c);
-                    }
-                    Input.nextLine();
+                    print();
                     break;
 
                 case 4:
                     //find contact by name
-                    int exist;
+                    
                     System.out.print("Enter contact name: ");
                     name = Input.nextLine();
-                    exist=findContact(name);
-                    if(exist==-1)
-                        System.out.println("Name not found.");
-                    else
-                        System.out.println(contactList.get(exist));
+                    findContactByName(name);
+                    
                     break;
 
                 case 5:
                     //sort contact list by name
-                    Collections.sort(contactList, new NameComperator());
+                    sortByName();
+                    
                     break;
 
                 case 6:
                     //sort contact list by number
-                    Collections.sort(contactList, new NumberComperator());
+                    sortByNumber();
+                    
                     break;
 
                 case 7:
-                    //reverse the exsisting list
-                    for(int i = 0; i < contactList.size()/2; i++)
-                    {
-                        Contact temp = contactList.get(i);
-                        contactList.set(i,contactList.get(contactList.size()-(i+1)));
-                        contactList.set(contactList.size()-(i+1),temp);
-                    }
+                    reverseList();
                     System.out.println("Reversed list order");
                     break;
 
@@ -119,47 +87,14 @@ public class Phonebook extends App
                     //save to file
                     System.out.print("Enter file name: ");
                     filename = Input.nextLine();
-                    try {
-                        File f = new File(filename);
-                        if (f.createNewFile()) {
-                          System.out.println("File created: " + f.getName());
-                        } 
-                        //in case file already exists
-                        else {
-                          System.out.println("File already exists.");
-                        }
-                        FileWriter fWriter = new FileWriter(f);
-                        //copy all contact list to txt file
-                        for(Contact c : contactList){
-                            fWriter.write(c.toString() + "\n");
-                        }
-                        fWriter.close();
-                      } catch (IOException e) {
-                        System.out.println("An error occurred.");
-                        e.printStackTrace();
-                      }
+                    writeToFile(filename);
                     break;
 
                 case 9:
                     //read from file
                     System.out.print("Enter file name: ");
                     filename = Input.nextLine();
-                    File f = new File(filename);
-                    try {
-                        Scanner filereader = new Scanner(f);
-                        //as long as file isn't finished
-                        while (filereader.hasNextLine()) 
-                        {
-                            String data = filereader.nextLine();
-                            String[] splitdata = data.split(",");
-                            contactList.add(new Contact(splitdata[0], splitdata[1]));
-                        }
-                        filereader.close();
-                        set(contactList);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                    removeDup();
+                    readFromFile(filename);
                     break;
                  
                 case 10:
@@ -200,9 +135,135 @@ public class Phonebook extends App
         super.update();
     }
 
+    public void addContact(String name,String number)
+    {
+        //find if contact already exists
+        if (contactList!=null)
+        {
+        if(findContact(name)!=-1)
+        {
+        System.out.print("Contact already exists.");
+        
+        } 
+        else 
+        contactList.add(new Contact(name, number));
+        set(contactList);
+        }
+
+        
+        
+
+
+    }
+     
+    public void removeContact(String name)
+    {
+        for (int i = 0; i < contactList.size(); i++) {
+            if(name.equals(contactList.get(i).name))
+            {
+                contactList.remove(i);
+                break;
+            }
+        }
+        set(contactList);
+
+    }
+
+    public void findContactByName(String name)
+    {
+        int exist;
+        exist=findContact(name);
+        if(exist==-1)
+            System.out.println("Name not found.");
+        else
+            System.out.println(contactList.get(exist));
+    }
+    public void sortByNumber()
+    {
+        Collections.sort(contactList, new NumberComperator());
+    }
+    public void sortByName()
+    {
+        Collections.sort(contactList, new NameComperator());
+    }
+
+    public void reverseList()
+    {
+       //reverse the exsisting list
+       for(int i = 0; i < contactList.size()/2; i++)
+       {
+           Contact temp = contactList.get(i);
+           contactList.set(i,contactList.get(contactList.size()-(i+1)));
+           contactList.set(contactList.size()-(i+1),temp);
+       }
+
+
+
+    }
+
+     public void writeToFile(String filename)
+     {
+
+        try {
+            File f = new File(filename);
+            if (f.createNewFile()) {
+              System.out.println("File created: " + f.getName());
+            } 
+            //in case file already exists
+            else {
+              System.out.println("File already exists.");
+            }
+            FileWriter fWriter = new FileWriter(f);
+            //copy all contact list to txt file
+            for(Contact c : contactList){
+                fWriter.write(c.toString() + "\n");
+            }
+            fWriter.close();
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+
+
+
+
+     }
+
+    public void readFromFile(String filename)
+    {
+        File f = new File(filename);
+        try {
+            Scanner filereader = new Scanner(f);
+            //as long as file isn't finished
+            while (filereader.hasNextLine()) 
+            {
+                String data = filereader.nextLine();
+                String[] splitdata = data.split(",");
+                contactList.add(new Contact(splitdata[0], splitdata[1]));
+            }
+            filereader.close();
+            set(contactList);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        removeDup();
+
+
+
+
+
+    }
+
+
+
     public void print()
     {
-        //TBI
+        //print all contacts
+        for(Contact c: contactList)
+        {
+            System.out.println(c);
+        }
+        Input.nextLine();
     }
 }
 
